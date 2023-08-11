886ccfedf6bf7243168ec22047ee753ff96d88ec
Anchal Kawale <20je0125@mech.iitism.ac.in>
2023-08-11 16:13:26 +0530

    changed frontend url

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://proventry-anchal0506.vercel.app/"],
+    origin: ["http://localhost:3000", "https://proventry-anchal0506.vercel.app"],
     credentials: true,
   })
 );
